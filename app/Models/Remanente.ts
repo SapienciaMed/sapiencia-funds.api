6e2b023bf39ff25b816d99592b7e4a28cf7b6100
Carlos Holguin <cholguin@i4digital.com>
2024-01-04 10:53:06 -0500

    Se refactoriza modelo y migracion de remanentes

--- app/Models/Remanente.ts
+++ app/Models/Remanente.ts
@@ -2,15 +2,64 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Remanente extends BaseModel {
 
-  public static table = "RMT_REMANETES";
+  public static table = "ECO_EXCEDENTES_CONTRATOS";
 
-  @column({ isPrimary: true, columnName: "RMT_CODIGO", serializeAs: "id" })
+  @column({ isPrimary: true, columnName: "ECO_CODIGO", serializeAs: "id" })
   public id: number;
 
-  @column({ columnName: "RMT_CONVOCATORIA", serializeAs: "announcement" })
+  @column({ columnName: "ECO_CONVOCATORIA", serializeAs: "announcement" })
   public announcement: number;
 
-  @column({ columnName: "RMT_FIDUCIA", serializeAs: "trust" })
+  @column({ columnName: "ECO_LINEA_CREDITO", serializeAs: "creditLine" })
+  public creditLine: string;
+
+  @column({ columnName: "ECO_CONTRATO", serializeAs: "contract" })
+  public contract: number;
+  
+  @column({ columnName: "ECO_NUMERO_BENEFICIOS", serializeAs: "numberOfBenefits" })
+  public numberOfBenefits: number;
+  
+  @column({ columnName: "ECO_VALOR_INVERSIÓN_CONTRATO", serializeAs: "contractInvestmentValue" })
+  public contractInvestmentValue: number;
+  
+  @column({ columnName: "ECO_VALOR_PROYECTADO", serializeAs: "projectedValue" })
+  public projectedValue: number;
+  
+  @column({ columnName: "ECO_VALOR_COMPROMETIDO_PROYECTADO", serializeAs: "ProjectedCommittedValue" })
+  public ProjectedCommittedValue: number;
+  
+  @column({ columnName: "ECO_VALOR_EXCEDENTE", serializeAs: "surplusValue" })
+  public surplusValue: number;
+  
+  @column({ columnName: "ECO_GIRADO", serializeAs: "turned" })
+  public turned: number;
+  
+  @column({ columnName: "ECO_PENDIENTE_POR_GIRAR_COMPROMETIDO", serializeAs: "pendingToBeTurnedCommitted" })
+  public pendingToBeTurnedCommitted: number;
+  
+  @column({ columnName: "ECO_PENDIENTE_POR_GIRAR_PROYECTADO", serializeAs: "projectedSlopeToTurn" })
+  public projectedSlopeToTurn: number;
+  
+  @column({ columnName: "ECO_RECURSOS_SIN_EJECUCIÓN", serializeAs: "resourcesWithoutExecution" })
+  public resourcesWithoutExecution: number;
+
+  @column({ columnName: "ECO_USUARIO_MODIFICO", serializeAs: "userModified" })
+  public userModified: string;
+
+  @column({ columnName: "ECO_FECHA_MODIFICO", serializeAs: "dateModified" })
+  public dateModified: Date;
+
+  @column({ columnName: "ECO_USUARIO_CREO", serializeAs: "userCreate" })
+  public userCreate: string;
+
+  @column({ columnName: "ECO_FECHA_CREO", serializeAs: "dateCreate" })
+  public dateCreate: Date;
+
+
+
+
+
+  /* @column({ columnName: "RMT_FIDUCIA", serializeAs: "trust" })
   public trust: number;
   
   @column({ columnName: "RMT_FONDO_COMUNA", serializeAs: "communityFund" })
@@ -44,7 +93,7 @@ export default class Remanente extends BaseModel {
   public dateCreate: Date;
   
   @column({ columnName: "RMT_CODPMA_PROGRAMA", serializeAs: "idProgram" })
-  public idProgram: number;
+  public idProgram: number; */
 
 
 
--- database/migrations/1702666538241_remnants.ts
+++ database/migrations/1702666538241_remnants.ts
@@ -1,16 +1,110 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'RMT_REMANETES'
+  //protected tableName = 'RMT_REMANETES'
+  protected tableName = 'ECO_EXCEDENTES_CONTRATOS'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
 
       table.comment(
-        "Tabla que contiene los calculos de los remanentes"
+        "Tabla que contiene los calculos de los excedentes de contratos"
       );
 
-      table.increments('RMT_CODIGO')
+      table.increments('ECO_CODIGO')
+        .unsigned()
+        .primary()
+        .comment("llave primaria");
+
+      table
+        .string("ECO_CONVOCATORIA", 10)
+        .notNullable()
+        .comment("Numero de la convocatoria");
+
+      table
+        .string("ECO_LINEA_CREDITO", 10)
+        .notNullable()
+        .comment("Linea de credito o beca");
+
+      table
+        .integer("ECO_CONTRATO")
+        .notNullable()
+        .comment("Codigo de contrato");
+
+      table
+        .integer("ECO_NUMERO_BENEFICIOS")
+        .notNullable()
+        .comment("Numero de beneficios");
+
+      table
+        .decimal("ECO_VALOR_INVERSIÓN_CONTRATO", 15, 2)
+        .notNullable()
+        .comment("Valor de inversion de contrato");
+
+      table
+        .decimal("ECO_VALOR_PROYECTADO", 15, 2)
+        .notNullable()
+        .comment("Valor proyectado");
+
+      table
+        .decimal("ECO_VALOR_COMPROMETIDO_PROYECTADO", 15, 2)
+        .notNullable()
+        .comment("Valor comprometido proyectado");
+
+      table
+        .decimal("ECO_VALOR_EXCEDENTE", 15, 2)
+        .notNullable()
+        .comment("Valor excedente");
+
+      table
+        .decimal("ECO_GIRADO", 15, 2)
+        .notNullable()
+        .comment("Valor girado");
+
+      table
+        .decimal("ECO_PENDIENTE_POR_GIRAR_COMPROMETIDO", 15, 2)
+        .notNullable()
+        .comment("Valor pendiente por girar comprometido");
+
+      table
+        .decimal("ECO_PENDIENTE_POR_GIRAR_PROYECTADO", 15, 2)
+        .notNullable()
+        .comment("Valor pendiente por girar proyectado");
+
+      table
+        .decimal("ECO_RECURSOS_SIN_EJECUCIÓN", 15, 2)
+        .notNullable()
+        .comment("Recursos sin ejecucion");
+
+      table
+        .string("ECO_USUARIO_MODIFICO", 15)
+        .nullable()
+        .comment("Numero del documento del ultimo usuario que hizo una modificacion");
+
+      table
+        .dateTime("ECO_FECHA_MODIFICO")
+        .nullable()
+        .comment("Fecha y hora de la ultima modificacion");
+
+      table
+        .string("ECO_USUARIO_CREO", 15)
+        .notNullable()
+        .comment("Numero del documento del usuario que creo el registro");
+
+      table
+        .dateTime("ECO_FECHA_CREO")
+        .notNullable()
+        .comment("Fecha y hora de creacion del registro");
+
+
+
+
+
+
+
+
+
+      /* table.increments('RMT_CODIGO')
         .unsigned()
         .primary()
         .comment("llave primaria");
@@ -82,7 +176,7 @@ export default class extends BaseSchema {
         .unsigned()
         .notNullable()
         .comment("Codigo Programa (FK PMA)");
-
+*/
     })
 
   }
