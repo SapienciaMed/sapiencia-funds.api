6b56082d768225a0b1a46d000d84c7cbc5ceae34
Sebastian Medina <jmedina@i4digital.com>
2024-01-04 12:54:31 -0500

    Ajuste de periodo de gracia en el resultado paginado de listado de reglamentos

--- app/Repositories/ReglamentConsolidationRepository.ts
+++ app/Repositories/ReglamentConsolidationRepository.ts
@@ -133,14 +133,19 @@ export default class ReglamentConsolidationRepository implements IReglamentConso
         knowledgeTransferCondonationType: iter.knowledgeTransferCondonationType,
         knowledgeTransferCondonationPercent: arrayConvertKnowledgeTransferCondonationPercent,
 
+        applyGracePeriod: iter.applyGracePeriod,
         gracePeriodMonths: iter.gracePeriodMonths,
         graceDateApplication: iter.graceDateApplication,
+
         applyContinuousSuspension: iter.applyContinuousSuspension,
         continuosSuspencionQuantity: iter.continuosSuspencionQuantity,
+
         applyDiscontinuousSuspension: iter.applyDiscontinuousSuspension,
         discontinuousSuspensionQuantity: iter.discontinuousSuspensionQuantity,
+
         applySpecialSuspensions: iter.applySpecialSuspensions,
         specialSuspensionsQuantity: iter.specialSuspensionsQuantity,
+
         applyExtension: iter.applyExtension,
         extensionQuantity: iter.extensionQuantity,
 
